d50d157689ca5ee775c147423d6bb046b90dee3e
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-01 15:29:20 +0530

    Coonected to DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
-
