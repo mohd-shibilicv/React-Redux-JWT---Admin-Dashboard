64dbe5d548e5c5c9e30d59d4a0d2e2c0077e2808
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-01 17:52:51 +0530

    Added a middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -23,5 +26,15 @@ app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
