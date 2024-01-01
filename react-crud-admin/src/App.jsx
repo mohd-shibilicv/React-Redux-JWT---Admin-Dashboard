65b62b1cc4da9a9e417a5ce86a795e1c5a055b56
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-01 12:28:01 +0530

    Add routes and create pages

--- react-crud-admin/src/App.jsx
+++ react-crud-admin/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
