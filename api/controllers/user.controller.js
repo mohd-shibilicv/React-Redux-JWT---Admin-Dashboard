3ed867e0de407fdf394f8b0b8c980a591d5a746f
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-01 16:32:04 +0530

    Create test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    messages: "API 200 OK",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -17,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+app.use('/api/user', userRoutes);
