a3036c9ed9eb2e97ec3576411222ca46a62f9e46
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-04 18:05:46 +0530

    Update User API Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -21,6 +22,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
