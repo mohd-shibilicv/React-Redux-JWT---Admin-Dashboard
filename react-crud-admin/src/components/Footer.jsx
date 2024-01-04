9f6dbf794633bc074410819d31067a4acc26dc82
mohammed shibili cv <mohshibilicv@gmail.com>
2024-01-04 17:35:52 +0530

    Upload Image Functionality added

--- react-crud-admin/src/components/Footer.jsx
+++ react-crud-admin/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Footer({ pos }) {
+export default function Footer() {
   return (
-    <footer className="absolute bottom-0 flex w-full font-semibold flex-col items-cente text-center text-black">
+    <footer className="relative bottom-0 flex w-full font-semibold flex-col items-cente text-center text-black">
       <div className="w-full p-4 text-center text-gray-700 dark:text-gray-200">
         © 2023 Copyright &nbsp;
         <a
